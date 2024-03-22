b90ce251badc6b4feb0ee3237313aa3da180cf01
Carlos Eduardo Amorim <86995782+eduardoamorim-dev@users.noreply.github.com>
2024-03-22 19:16:03 -0300

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -3,7 +3,7 @@ use bank;
 
 CREATE TABLE banco (
     codigo_banco INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(45),
+    nome VARCHAR(45)
 );
 
 CREATE TABLE agencia (
@@ -11,38 +11,38 @@ CREATE TABLE agencia (
     cod_banco INT NOT NULL,
     FOREIGN KEY (cod_banco)
         REFERENCES banco (codigo_banco)
-        ON UPDATE RESTRICT ON DELETE RESTRICT
+        ON UPDATE RESTRICT ON DELETE RESTRICT,
     endereco VARCHAR(100)
 );
 
 CREATE TABLE conta (
     num_conta VARCHAR(7) NOT NULL PRIMARY KEY,
-    saldo FLOAT NOT NULL;
-    tipo_conta INT;
+    saldo FLOAT NOT NULL,
+    tipo_conta INT,
     num_agencia INT NOT NULL,
     FOREIGN KEY (num_agencia)
-        REFERENCES conta (num_agencia)
+        REFERENCES agencia (numero_agencia)
 );
 
 
 CREATE TABLE cliente (
-    cpf VARCHAR(14) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cpf VARCHAR(14) NOT NULL PRIMARY KEY,
     nome VARCHAR(45) NOT NULL,
     endereco  VARCHAR(100),
     sexo CHAR(1)
-)
+);
 
 
 CREATE TABLE historico (
     historico_cpf VARCHAR(14) NOT NULL,
     FOREIGN KEY (historico_cpf)
-        REFERENCES cliente (cpf)
-        ON UPDATE RESTRICT ON DELETE RESTRICT,
+        REFERENCES cliente (cpf),
     historico_num_conta VARCHAR(7) NOT NULL,
-    FOREIGN KEY (historico_num_contf)
+    FOREIGN KEY (historico_num_conta)
         REFERENCES conta (num_conta)
         ON UPDATE RESTRICT ON DELETE RESTRICT,
-    data_inicio DATE
+    data_inicio DATE,
+    CONSTRAINT cpf_num_conta PRIMARY KEY historico(historico_cpf, historico_num_conta)
 );
 
 CREATE TABLE telefone_cliente (
@@ -50,6 +50,5 @@ CREATE TABLE telefone_cliente (
     FOREIGN KEY (telefone_cpf_cliente)
         REFERENCES cliente (cpf)
         ON UPDATE RESTRICT ON DELETE RESTRICT,
-   telefone VARCHAR(20) NOT NULL PRIMARY KEY,
-);
-
+   telefone VARCHAR(20) NOT NULL PRIMARY KEY
+); 
