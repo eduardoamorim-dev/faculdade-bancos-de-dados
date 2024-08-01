fda72d85a76c5fd1778c76499e6f24a9b845a42e
Carlos Eduardo Amorim <86995782+eduardoamorim-dev@users.noreply.github.com>
2024-08-01 19:33:38 -0300

    Update Atv06.sql

--- Atv06.sql
+++ Atv06.sql
@@ -1,31 +1,33 @@
 -- SHOW grants;
 -- 1. Crie um usuário my_user1;
-CREATE USER my_usser1 IDENTIFIED BY "123456778";
+CREATE USER my_user1 IDENTIFIED BY "123456778";
 
 -- 2. Crie um usuário my_admin;
 CREATE USER my_admin IDENTIFIED BY "eduardoamorim";
 
 -- 3. Crie/use dois bancos de dados bd1=universidade e db2 quaisquer. Pode ser bancos de outros exercícios. Crie algumas tabelas aleatoriamente.
+
+
 -- 4. Conceda acesso total para o usuário my_admin ao banco bd1.
 GRANT ALL ON universidade.* TO my_admin;
 
 -- 5. Conceda apenas select, insert, delete e update para o usuário my_user no bd1.
-GRANT SELECT, INSERT, DELETE, UPDATE ON universidade.* TO my_usser1;
+GRANT SELECT, INSERT, DELETE, UPDATE ON universidade.* TO my_user1;
 
 -- 6. Visualize as permissões dadas aos usuários.
-SHOW GRANTS FOR my_usser1;
+SHOW GRANTS FOR my_user1;
 SHOW GRANTS FOR my_admin;
 
 -- 7. Retire todas as permissões do usuário my_user.
-REVOKE SELECT, INSERT, DELETE, UPDATE ON universidade.* FROM my_usser1;
+REVOKE SELECT, INSERT, DELETE, UPDATE ON universidade.* FROM my_user1;
 
 -- 8. Crie uma nova conexão com o usuário my_user e use o comando show tables;
-GRANT SELECT ON universidade.* TO my_usser1;
+GRANT SELECT ON universidade.* TO my_user1;
 SHOW TABLES;
 
 -- 9. Tente fazer uma operação de update usando o usuário my_user.
 -- Conceda permissões de UPDATE ao usuário my_usser1:
-GRANT UPDATE ON universidade.* TO my_usser1;
+GRANT UPDATE ON universidade.* TO my_user1;
 -- Saia do MySQL e conecte-se novamente usando o usuário my_usser1:
 Use universidade;
 UPDATE aluno SET nome = 'Natalia S' WHERE id_aluno = 1;
@@ -56,6 +58,7 @@ UPDATE aluno SET nome = 'Admin Nat' WHERE id = 1;
 -- Verifique as tabelas no banco de dados universidade:
 USE universidade;
 SHOW TABLES;
+
 -- Tente fazer uma operação de UPDATE:
 UPDATE estudantes SET nome = 'Dev Nat' WHERE id = 2;
 SHOW GRANTS FOR papelAdmin;
